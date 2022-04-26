637122e1be7e31994baec30c0d9ae70e5671f545
HagarAbouroumia <48414362+HagarAbouroumia@users.noreply.github.com>
2022-04-26 17:52:28 +0200

    Update extract_classes.py

--- scripts/extract_classes.py
+++ scripts/extract_classes.py
@@ -7,13 +7,15 @@ if __name__ == "__main__":
     
 #     classes_22 for province while classes_12 is for country
     # path = "NADI2021_DEV.1.0/NADI2021_DEV.1.0/Subtask_1.2+2.2_DA/DA_dev_labeled.tsv"
-    path = "/content/NADI2021_DEV.1.0/NADI2021_DEV.1.0/Subtask_1.2+2.2_DA/DA_train_labeled.tsv"
+#     path = "/content/NADI2021_DEV.1.0/NADI2021_DEV.1.0/Subtask_1.2+2.2_DA/DA_train_labeled.tsv"
+    path = "/content/QADI/train_set.csv"
     df = pd.read_csv(path, delimiter='\t')
     class_name = "classes_12.txt"
     labels_1 = sorted(df["#3_country_label"].unique().tolist())
-    labels_2 = sorted(df["#4_province_label"].unique().tolist())
+#     labels_2 = sorted(df["#4_province_label"].unique().tolist())
 
-    print(f"n1={len(labels_1)} | n2={len(labels_2)}")
+#     print(f"n1={len(labels_1)} | n2={len(labels_2)}")
+    print(f"n1={len(labels_1)}")
 
     path = os.path.join(os.path.dirname(path), class_name)
     with open(path, 'w') as fout:
